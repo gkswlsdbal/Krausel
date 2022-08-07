31b2b3372c04534887bf70d45a43979dbab9fdf0
gkswls1117@gmail.com <gkswls1117@gmail.com>
2022-08-07 20:19:53 +0900

    spark install

--- Manager/build.sbt
+++ Manager/build.sbt
@@ -6,3 +6,14 @@ lazy val root = (project in file("."))
   .settings(
     name := "Manager"
   )
+
+name:="spark-sample"
+
+version:="0.1"
+
+scalaVersion:="2.13.8"
+
+val sparkVersion="3.2.0"
+
+libraryDependencies ++=Seq("org.apache.spark" %% "spark-core" % sparkVersion,
+  "org.apache.spark" %% "spark-sql" % sparkVersion)
--- Manager/src/main/scala/Main.scala
+++ Manager/src/main/scala/Main.scala
@@ -1,5 +1,15 @@
-object Main {
+import SparkUtil.SparkSessionWrapper
+
+object Main extends SparkSessionWrapper {
   def main(args: Array[String]): Unit = {
-    println("Hello world!")
+    import spark.implicits._
+
+    val textFile = spark.read.textFile("/home/hanjin/git/Krausel/DataSet/words.txt")
+    val text =spark.read.csv("/home/hanjin/git/Krausel/DataSet/bigeastncaabasketball.csv")
+    val wordCounts = textFile.flatMap(line => line.split(" "))
+      .groupByKey(identity)
+      .count()
+
+    text.show()
   }
 }
